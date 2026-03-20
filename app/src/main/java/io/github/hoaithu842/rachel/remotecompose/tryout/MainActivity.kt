3e9a9aadbf3d202dafd6d4dcc221d021df33a3bd
hoaithu842 <hoaithu842@gmail.com>
2026-03-20 14:35:18 +0700

    Inital commit

---
+++ app/src/main/java/io/github/hoaithu842/rachel/remotecompose/tryout/MainActivity.kt
@@ -0,0 +1,47 @@
+package io.github.hoaithu842.rachel.remotecompose.tryout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.hoaithu842.rachel.remotecompose.tryout.ui.theme.RachelRemoteComposeTryoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RachelRemoteComposeTryoutTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RachelRemoteComposeTryoutTheme {
+        Greeting("Android")
+    }
+}
